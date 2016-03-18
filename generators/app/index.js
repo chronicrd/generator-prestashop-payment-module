e477f57388e0cf43b80e95201cb067510891496f
Marc Picaud <picaud.marc@gmail.com>
2016-03-18 21:32:09 +0100

    Use 2 spaces for <TAB> & other code style fix

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,141 +5,143 @@ var yosay = require('yosay');
 var mkdirp = require('mkdirp');
 
 module.exports = yeoman.generators.Base.extend({
-    prompting: function () {
-        var done = this.async();
+  prompting: function () {
+    var done = this.async();
 
-        // Have Yeoman greet the user.
-        this.log(yosay(
-            'Welcome to the terrific ' + chalk.red('Payment Module') + ' generator!'
-        ));
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the terrific ' + chalk.red('Payment Module') + ' generator!'
+    ));
 
-        // Prompt the name of the module
-        var prompts = [{
-            type: 'input',
-            name: 'technicalName',
-            message: 'Technical Name (lowercase)',
-            default: 'mymodule'
-        },
-        {
-            type: 'input',
-            name: 'className',
-            message: 'Class Name',
-            default: 'className', 
-        },
-        {
-            type: 'input',
-            name: 'displayName',
-            message: 'Display Name',
-            default: 'mymodule'
-        },
-        {
-            type: 'input',
-            name: 'author',
-            message: 'Author',
-            default: 'Yeoman'
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'description',
-            default: 'Awesome module'
-        },
-        {
-            type: 'input',
-            name: 'paymentProviderClass',
-            message: 'Payment provider class name (in camelCase)',
-            default: 'PaymentProvider'
-        }];
+    // Prompt the name of the module
+    var prompts = [
+      {
+        type: 'input',
+        name: 'technicalName',
+        message: 'Technical Name (lowercase)',
+        default: 'mymodule'
+      },
+      {
+        type: 'input',
+        name: 'className',
+        message: 'Class Name',
+        default: 'className'
+      },
+      {
+        type: 'input',
+        name: 'displayName',
+        message: 'Display Name',
+        default: 'mymodule'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Author',
+        default: 'Yeoman'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'description',
+        default: 'Awesome module'
+      },
+      {
+        type: 'input',
+        name: 'paymentProviderClass',
+        message: 'Payment provider class name (in camelCase)',
+        default: 'PaymentProvider'
+      }
+    ];
 
-        this.prompt(prompts, function (props) {
-            this.props = props;
-            // To access props later use this.props.someOption;
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
 
-            done();
-        }.bind(this));
-    },
+      done();
+    }.bind(this));
+  },
 
-    writing: function () {
-        var module_path = this.destinationRoot() + '/' + this.props.technicalName;
+  writing: function () {
+    var modulePath = this.destinationRoot() + '/' + this.props.technicalName;
 
-        // Creates the module hierarchy
-        mkdirp(module_path);
-        mkdirp(module_path + '/' + 'classes');
-        mkdirp(module_path + '/' + 'controllers/front');
-        mkdirp(module_path + '/' + 'controllers/back');
-        mkdirp(module_path + '/' + 'views/css');
-        mkdirp(module_path + '/' + 'views/js');
-        mkdirp(module_path + '/' + 'views/templates/admin');
-        mkdirp(module_path + '/' + 'views/templates/front');
-        mkdirp(module_path + '/' + 'views/templates/hooks');
+    // Creates the module hierarchy
+    mkdirp(modulePath);
+    mkdirp(modulePath + '/classes');
+    mkdirp(modulePath + '/controllers/front');
+    mkdirp(modulePath + '/controllers/back');
+    mkdirp(modulePath + '/views/css');
+    mkdirp(modulePath + '/views/js');
+    mkdirp(modulePath + '/views/templates/admin');
+    mkdirp(modulePath + '/views/templates/front');
+    mkdirp(modulePath + '/views/templates/hooks');
 
-        // Generates files
-        this.fs.copyTpl(
-            this.templatePath('mymodule.php'),
-            module_path + '/' + this.props.technicalName + '.php', {
-                technicalName: this.props.technicalName,
-                paymentProviderClass: this.props.paymentProviderClass,
-                className: this.props.className,
-                displayName: this.props.displayName,
-                author: this.props.author,
-                description: this.props.description
-            }
-        );
+    // Generates files
+    this.fs.copyTpl(
+      this.templatePath('mymodule.php'),
+      modulePath + '/' + this.props.technicalName + '.php', {
+        technicalName: this.props.technicalName,
+        paymentProviderClass: this.props.paymentProviderClass,
+        className: this.props.className,
+        displayName: this.props.displayName,
+        author: this.props.author,
+        description: this.props.description
+      }
+    );
 
-        this.fs.copyTpl(
-            this.templatePath('README.md'),
-            module_path + '/README.md', {
-                technicalName: this.props.technicalName,
-            }
-        );
+    this.fs.copyTpl(
+      this.templatePath('README.md'),
+      modulePath + '/README.md', {
+        technicalName: this.props.technicalName
+      }
+    );
 
-        this.fs.copyTpl(
-            this.templatePath('classes/PaymentProviderApi.php'),
-            module_path + '/classes/' + this.props.paymentProviderClass + '.php', {
-                paymentProviderClass: this.props.paymentProviderClass
-            }
-        );
+    this.fs.copyTpl(
+      this.templatePath('classes/PaymentProviderApi.php'),
+      modulePath + '/classes/' + this.props.paymentProviderClass + '.php', {
+        paymentProviderClass: this.props.paymentProviderClass
+      }
+    );
 
-        this.fs.copyTpl(
-            this.templatePath('controllers/front/redirect.php'),
-            module_path + '/controllers/front/redirect.php', {
-                paymentProviderClass: this.props.paymentProviderClass,
-                className: this.props.className
-            }
-        );
+    this.fs.copyTpl(
+      this.templatePath('controllers/front/redirect.php'),
+      modulePath + '/controllers/front/redirect.php', {
+        paymentProviderClass: this.props.paymentProviderClass,
+        className: this.props.className
+      }
+    );
 
-        this.fs.copy(
-            this.templatePath('views/css/back.css'),
-            module_path + '/views/css/back.css'
-        );
+    this.fs.copy(
+      this.templatePath('views/css/back.css'),
+      modulePath + '/views/css/back.css'
+    );
 
-        this.fs.copy(
-            this.templatePath('views/css/front.css'),
-            module_path + '/views/css/front.css'
-        );
+    this.fs.copy(
+      this.templatePath('views/css/front.css'),
+      modulePath + '/views/css/front.css'
+    );
 
-        this.fs.copy(
-            this.templatePath('views/js/back.js'),
-            module_path + '/views/js/back.js'
-        );
+    this.fs.copy(
+      this.templatePath('views/js/back.js'),
+      modulePath + '/views/js/back.js'
+    );
 
-        this.fs.copy(
-            this.templatePath('views/js/front.js'),
-            module_path + '/views/js/front.js'
-        );
+    this.fs.copy(
+      this.templatePath('views/js/front.js'),
+      modulePath + '/views/js/front.js'
+    );
 
-        this.fs.copyTpl(
-            this.templatePath('views/templates/front/redirect.tpl'),
-            module_path + '/views/templates/front/redirect.tpl', {
-                technicalName: this.props.technicalName
-            }
-        );
+    this.fs.copyTpl(
+      this.templatePath('views/templates/front/redirect.tpl'),
+      modulePath + '/views/templates/front/redirect.tpl', {
+        technicalName: this.props.technicalName
+      }
+    );
 
-        this.fs.copyTpl(
-            this.templatePath('views/templates/hooks/payment.tpl'),
-            module_path + '/views/templates/hooks/payment.tpl', {
-                technicalName: this.props.technicalName
-            }
-        );
-    },
+    this.fs.copyTpl(
+      this.templatePath('views/templates/hooks/payment.tpl'),
+      modulePath + '/views/templates/hooks/payment.tpl', {
+        technicalName: this.props.technicalName
+      }
+    );
+  }
 });
--- test/app.js
+++ test/app.js
@@ -6,16 +6,16 @@ var helpers = require('yeoman-generator').test;
 describe('generator-prestashop-payment-module:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({someOption: true})
-      .withPrompts({
-          technicalName: 'dummymodule',
-          className: 'DummyModule',
-          displayName: 'Dummy Module',
-          author: 'Marc Picaud',
-          description: 'A very dummy module',
-          paymentProviderClass: 'DummyPaymentProviderClass'
-      })
-      .on('end', done);
+    .withOptions({someOption: true})
+    .withPrompts({
+      technicalName: 'dummymodule',
+      className: 'DummyModule',
+      displayName: 'Dummy Module',
+      author: 'Marc Picaud',
+      description: 'A very dummy module',
+      paymentProviderClass: 'DummyPaymentProviderClass'
+    })
+    .on('end', done);
   });
 
   it('generates the module main PHP file', function () {
@@ -24,22 +24,22 @@ describe('generator-prestashop-payment-module:app', function () {
     ]);
   });
 
-  it('generates the payment provider class file', function() {
-      assert.file([
-          'dummymodule/classes/DummyPaymentProviderClass.php'
-      ]);
+  it('generates the payment provider class file', function () {
+    assert.file([
+      'dummymodule/classes/DummyPaymentProviderClass.php'
+    ]);
   });
 
-  it('generates a front controller system to redirect clients towards the payment provider', function() {
-      assert.file([
-          'dummymodule/controllers/front/redirect.php',
-          'dummymodule/views/templates/front/redirect.tpl'
-      ]);
+  it('generates a front controller system to redirect clients towards the payment provider', function () {
+    assert.file([
+      'dummymodule/controllers/front/redirect.php',
+      'dummymodule/views/templates/front/redirect.tpl'
+    ]);
   });
 
-  it('generates a template payment.tpl', function() {
-      assert.file([
-          'dummymodule/views/templates/hooks/payment.tpl'
-      ]);
+  it('generates a template payment.tpl', function () {
+    assert.file([
+      'dummymodule/views/templates/hooks/payment.tpl'
+    ]);
   });
 });
