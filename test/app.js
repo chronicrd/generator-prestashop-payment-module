5791c50f4baa3a08ad9b86f9052ce2e640250c90
Marc Picaud <picaud.marc@gmail.com>
2016-03-18 21:08:04 +0100

    add some tests

--- test/app.js
+++ test/app.js
@@ -7,13 +7,39 @@ describe('generator-prestashop-payment-module:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
       .withOptions({someOption: true})
-      .withPrompts({someAnswer: true})
+      .withPrompts({
+          technicalName: 'dummymodule',
+          className: 'DummyModule',
+          displayName: 'Dummy Module',
+          author: 'Marc Picaud',
+          description: 'A very dummy module',
+          paymentProviderClass: 'DummyPaymentProviderClass'
+      })
       .on('end', done);
   });
 
-  it('creates files', function () {
+  it('generates the module main PHP file', function () {
     assert.file([
-      'dummyfile.txt'
+      'dummymodule/dummymodule.php'
     ]);
+  });
+
+  it('generates the payment provider class file', function() {
+      assert.file([
+          'dummymodule/classes/DummyPaymentProviderClass.php'
+      ]);
+  });
+
+  it('generates a front controller system to redirect clients towards the payment provider', function() {
+      assert.file([
+          'dummymodule/controllers/front/redirect.php',
+          'dummymodule/views/templates/front/redirect.tpl'
+      ]);
+  });
+
+  it('generates a template payment.tpl', function() {
+      assert.file([
+          'dummymodule/views/templates/hooks/payment.tpl'
+      ]);
   });
 });
